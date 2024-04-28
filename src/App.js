8691f77daffc3855981d466372be803068258c42
AndreasHyldal <90606169+AndreasHyldal@users.noreply.github.com>
2024-04-28 20:45:40 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/navbar";
+import Pokedex from "./routes/pokedex";
+import About from "./routes/About";
+import Home from "./routes/Home";
+
+import { Outlet, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="logo.svg" className="App-logo" alt="logo" />
-        <p>
-          Jeg kan ændre i <code>src/App.js</code> og få det vist hos mig.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div className="content">
+        <Outlet />
+      </div>
     </div>
   );
 }
