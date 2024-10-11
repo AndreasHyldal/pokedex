dd7d8ff1dab1f48c719cf6e2240d707dea477e18
AndreasHyldal <90606169+AndreasHyldal@users.noreply.github.com>
2024-10-11 09:50:24 +0200

    testing stuff
    idk

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,14 +5,15 @@ const Home = () => {
   return (
     <div>
       <h2>Home Page</h2>
-      <p>This is how it works....</p>
+      <p>How it works:</p>
 <br />
 <br />
       <ul>
-        <li>Go To Pokedex with the button in the Navigation</li>
-        <li>Scroll pokemons in the PokeDex</li>
-        <li>See further information about each pokemon by clicking it in the pokedex</li>
-        <li>Search specific pokemons in the About page bi Name or #ID</li>
+        <li>Use the PokeDex button in the top right</li>
+        <li>Check out the different PokeMons</li>
+        <li>Get some cool extra information about the PokeMons by clicking their image</li>
+        <li>Want to find a specific PokeMon? Look it up in the About section with its name or #ID</li>
+        <li>Try looking up #25, it's a pretty cool Pokemon</li>
       </ul>
     </div>
   );
